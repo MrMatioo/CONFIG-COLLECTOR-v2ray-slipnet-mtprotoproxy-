b1e3cf39058f8ff478851fb191d3cc853885171c
MrMatioo <mr.mahdii.rahimi@gmail.com>
2026-06-05 21:35:13 +0330

    just 3 message will be recive

--- src/utils/collector.ts
+++ src/utils/collector.ts
@@ -80,7 +80,7 @@ export const collector = async (client: TelegramClient): Promise<void> => {
 
   for (const channel of shuffledChannels) {
     try {
-      const messages = await client.getMessages(channel, { limit: 5 });
+      const messages = await client.getMessages(channel, { limit: 3 });
 
       for (const msg of messages) {
         if (msg?.message && typeof msg.message === "string") {
